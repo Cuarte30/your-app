f8cfd8896b79e555bc4fe8315e8bc0ae575ceb0a
Cuarte30 <rodrigodb9610@gmail.com>
2023-09-27 00:38:06 -0300

    Tarea 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Encuestas from './components/Encuestas';
+import React, { useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [encuestas] = useState([
+  {
+    id: 1,
+    pregunta: '¿Cuál es tu color favorito?',
+    opciones: ['Rojo', 'Azul', 'Verde'],
+  },
+  {
+    id: 2,
+    pregunta: '¿Cuál es tu comida favorita?',
+    opciones: ['Pizza', 'Hamburguesa', 'Sushi','Asado'],
+  },
+  {
+    id: 3,
+    pregunta: '¿Cuál es tu deporte favorito?',
+    opciones: ['Futbol', 'Basquet', 'Voley','Rugby', 'Otros'],
+  },
+  // Puedes agregar más encuestas aquí si lo deseas
+]);
+
+return (
+  <div className="App">
+    <header className="App-header">
+     
+      <h1>Aplicación de Encuestas</h1>
+     
+      <Encuestas encuestas={encuestas} />
+    </header>
+  </div>
+);
 }
 
 export default App;
+
+
+
